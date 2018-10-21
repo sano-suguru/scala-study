e9d1a8aa369c5b3b28b0c9288049b4f4e5eb3803
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-21 17:12:18 +0900

    [update]末尾最適化

--- Factorial.scala
+++ Factorial.scala
@@ -1,6 +1,8 @@
 import scala.math.BigInt
 
-object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
-  println(factorial(1000))
+object FactorialTailRec extends App {
+  def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else factorial(i - 1, i * acc)
+  println(factorial(10000, 1))
 }
+
+// 再帰関数の戻り値が必ず再帰関数の呼び出し、または、値となるように再帰関数を実装すると、内部的にループの表現に変換される末尾再帰最適化が行われる
