5b02eabc80f34211838d6f918da3741f9da8a94e
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-22 19:49:43 +0900

    シンプルな文字列検索のアプリケーションの作成

---
+++ SimpleSearch.scala
@@ -0,0 +1,7 @@
+object SimpleSearch extends App {
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndexes = Seq()
+
+  println(s"出現箇所: ${matchIndexes}")
+}
