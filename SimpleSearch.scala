aa424d5f85cfbfdec45e948b77a73ae16a37e5a0
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-22 20:05:04 +0900

    「パターン」と一致した添字を取得

--- SimpleSearch.scala
+++ SimpleSearch.scala
@@ -7,9 +7,19 @@ object SimpleSearch extends App {
     var matchIndexes = Seq[Int]()
     for (i <- 0 until text.length) {
       val partText = text.slice(i, i + pattern.length)
-      println(partText)
+      if (isMatch(partText, pattern)) matchIndexes = matchIndexes :+ i
     }
     matchIndexes
+  }
+
+  def isMatch(textPart: Seq[Char], pattern: Seq[Char]): Boolean = {
+    var isMatch = true
+    for (i <- 0 to pattern.length - 1) {
+      if (textPart.length < pattern.length || textPart(i) != pattern(i)) {
+        isMatch = false
+      }
+    }
+    isMatch
   }
 
   println(s"出現箇所: ${matchIndexes}")
